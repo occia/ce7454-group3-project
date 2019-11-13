91ff630f468ca240ab7deab4c9ccab5ba4e13308
polinatolmach <polina.tolmach@gmail.com>
2019-11-13 23:59:36 +0800

    scrapy fixes

--- scrapy/instascraper/instascraper/spiders/quotes_spider.py
+++ scrapy/instascraper/instascraper/spiders/quotes_spider.py
@@ -1,15 +1,20 @@
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
-    name = "profiles"
-    country = 'russia'
-    file_path = '../../../data/profiles/usernames/%s.txt' %country
+    name = 'profiles'
+    country = 'canada'
+    # Hardcoded path of a country's users names
+    file_path = '../../../data/test/usernames/%s.txt' %country
     with open(file_path) as f:
-        start_urls = f.readlines()
+        start_urls = []
+        for u in f.readlines():
+            start_urls.append('https://imgtagram.com/u/' + u)
 
     def parse(self, response):
-        country = 'russia'
+        # Hardcoded name of the country
+        country = 'canada'
         for quote in response.css('div.text-block'):
+            # The HTML elements we are interested in
             yield {
                 'username': quote.css('h3::text').get(),
                 'name': quote.css('h1::text').get(),
@@ -17,4 +22,3 @@ class QuotesSpider(scrapy.Spider):
                 'image': response.css('img.icon::attr(src)').get(),
                 'country': country
             }
-
